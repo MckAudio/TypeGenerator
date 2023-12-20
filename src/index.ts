0c0e186193330f1d8a776b3771f41c2c1914484f
Henri Meissner <henri.meissner@impulse-audio-lab.com>
2023-12-20 10:54:36 +0100

    Added file export as JSON that includes all links

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { Dictionary, ClassType, SimpleType, ArrayType, LinkType, FileStore, FileType, IsSimpleType, EnumType, EnumDefinition } from "./types";
 import { ISerializer, SerializerMap } from './ISerializer';
 
-import fs from 'fs';
+import fs, { link, mkdirSync, writeFileSync } from 'fs';
 import { CppSerializer } from './CppSerializer';
 import { HppSerializer } from './HppSerializer';
 import { TsSerializer } from './TsSerializer';
@@ -22,8 +22,6 @@ String.prototype.isOneOf = function (this: string, keys: Array<string>): boolean
 
 let fileMap: Dictionary<FileStore> = {};
 let sources: Dictionary<SerializerMap> = {};
-
-let debug = true;
 
 const simpleTypes = ["signed", "unsigned", "float", "string", "bool", "boolean"];
 
@@ -142,6 +140,70 @@ function ReadFile(file: string) {
     fileMap[fileId].parsed = true;
 }
 
+function WriteJsonFile(file: string) {
+    let key = path.basename(file, path.extname(file));
+    let rootPath = path.join(path.dirname(file), "json");
+    if (fs.existsSync(rootPath) === false) {
+        fs.mkdirSync(rootPath);
+    }
+
+    if (fileMap.hasOwnProperty(key)) {
+        let fp = path.join(rootPath, path.basename(file));
+        let fd = fileMap[key];
+
+        let fo = new FileType();
+        fo.meta.author = "TypeGenerator";
+        fo.meta.namespace = fd.namespace;
+        fo.enums = fd.enums;
+        
+        Object.entries(fd.data).forEach(cIn => {
+            let cOut = new ClassType();
+            cOut.newProperty = cIn[1].newProperty;
+            cOut.parent = cIn[1].parent;
+
+            Object.entries(cIn[1].members).forEach(mIn => {
+                if (mIn[1].type === "link" && mIn[1].hasOwnProperty("file")) {
+                    // Do special stuff
+                    let linkFileName = mIn[1].file as string;
+                    let linkKey = path.basename(linkFileName, path.extname(linkFileName));
+                    if (fileMap.hasOwnProperty(linkKey)) {
+                        let newLinkName = `${linkKey}_${mIn[1].name}`;
+                        let mOut = new LinkType();
+                        mOut.newProperty = mIn[1].newProperty;
+                        mOut.name = newLinkName;
+                        cOut.members[mIn[0]] = mOut;
+
+                        // Add all members of linked file with prefix to our file
+                        if (fo.classes.hasOwnProperty(newLinkName) === false) {
+                            let fl = fileMap[linkKey];
+                            Object.entries(fl.enums).forEach(fle => (fo.enums as Dictionary<EnumDefinition>)[fle[0]] = fle[1] );
+                            Object.entries(fl.data).forEach(flc => {
+                                let cn = `${linkKey}_${flc[0]}`;
+                                fo.classes[cn] = Copy(flc[1]);
+                                Object.entries(fo.classes[cn].members).forEach(tm => {
+                                    if (tm[1].type === "link") {
+                                        tm[1].name = `${linkKey}_${tm[1].name}`;
+                                    } else if (tm[1].type === "array" && tm[1].items.type === "link") {
+                                        tm[1].items.name = `${linkKey}_${tm[1].items.name}`;
+                                    }
+                                });
+                            });
+                        }
+                    }
+                } else {
+                    cOut.members[mIn[0]] = Copy(mIn[1]);
+                }
+            });
+            fo.classes[cIn[0]] = cOut;
+        });
+
+
+        let out = JSON.stringify(fo, undefined, 4);
+        fs.writeFileSync(fp, out);
+    }
+
+}
+
 function WriteFileSources(fileId: string, outDir?: string) {
     let fileData = fileMap[fileId];
     let root = path.dirname(fileData.path);
@@ -162,23 +224,22 @@ function main(argc: number, argv: Array<any>) {
     }
 
     for (let i = 0; i < argc; i++) {
-        if (path.isAbsolute(argv[i])) {
-            ReadFile(argv[i]);
-        } else {
-            ReadFile(path.join(process.cwd(), argv[i]));
+        let fp = argv[i];
+        if (path.isAbsolute(fp) === false) {
+            fp = path.join(process.cwd(), argv[i]);
         }
+        ReadFile(fp);
+        WriteJsonFile(fp);
     }
 
-    if (debug) {
-        Object.entries(fileMap).forEach(entry => {
-            if (entry[1].parsed) {
-                console.log(`Successfully parsed file "${entry[0]}" in path "${entry[1].path}"`);
-                WriteFileSources(entry[0]);
-            } else {
-                console.log(`Failed to parse file "${entry[0]}" in path "${entry[1].path}"`);
-            }
-        });
-    }
+    Object.entries(fileMap).forEach(entry => {
+        if (entry[1].parsed) {
+            console.log(`Successfully parsed file "${entry[0]}" in path "${entry[1].path}"`);
+            WriteFileSources(entry[0]);
+        } else {
+            console.log(`Failed to parse file "${entry[0]}" in path "${entry[1].path}"`);
+        }
+    });
 }
 
 main(process.argv.length - 2, process.argv.slice(2));
